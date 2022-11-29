d8943c6f521fc646cc850f9537771b76461eae7b
Jefferson Pedro <jeffersonpedro05@gmail.com>
2022-11-29 10:59:09 -0300

    primeira estilização

--- onebitlife/src/Pages/Start/index.jsx
+++ onebitlife/src/Pages/Start/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { View, ScrollView, Text, Image } from "react-native";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
 
 
 export default function Start(){
 
 return(
-    <View>
+    <View style={styles.container}>
         <ScrollView showsVerticalScrollIndicator={false}>
-            <View>
-                <Image source={require ("../../assets/icons/logo3.png")}/>
-                <Text>
-                    Vamos transformar sua vida{"\n"} em jogo. buscando sempre {"\n"} o 
+            <View style= {{ alignItems: "center" }}>
+                <Image 
+                source={require ("../../assets/icons/logo3.png")}
+                style={styles.logo}
+                />
+                <Text style={styles.description}>
+                    Vamos transformar sua vida{"\n"} em jogo, buscando sempre {"\n"} o 
                     melhor nível.
                 </Text>
             </View>    
@@ -19,3 +22,22 @@ return(
 );
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+    description: {
+        color: "#FFFFFF",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+    },
+})
