8d723eabf7d4318f4854752cc6a845506c95d55e
Jefferson Pedro <jeffersonpedro05@gmail.com>
2022-11-29 11:44:26 -0300

    Inserindo animações e estruturando componentes

--- onebitlife/src/Pages/Start/index.jsx
+++ onebitlife/src/Pages/Start/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
+import LifeStatus from "../../Components/Common/LifeStatus/index.jsx";
 
 
 export default function Start(){
@@ -12,6 +13,7 @@ return(
                 source={require ("../../assets/icons/logo3.png")}
                 style={styles.logo}
                 />
+                <LifeStatus/>
                 <Text style={styles.description}>
                     Vamos transformar sua vida{"\n"} em jogo, buscando sempre {"\n"} o 
                     melhor nível.
