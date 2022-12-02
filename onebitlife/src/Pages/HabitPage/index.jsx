8603c9c68d478d9a02c62666c5266bcbeb59f5f4
Jefferson Pedro <jeffersonpedro05@gmail.com>
2022-12-02 00:54:15 -0300

    Linkando a pagina com o serviço create habit

--- onebitlife/src/Pages/HabitPage/index.jsx
+++ onebitlife/src/Pages/HabitPage/index.jsx
@@ -9,6 +9,7 @@ import Notification from "../../Components/HabitPage/Notification";
 import TimeDatePicker from "../../Components/HabitPage/TimeDataPicker";
 import UpdateExcludeButtons from "../../Components/HabitPage/UpdateExcludeButtons";
 import DefaultButton from "../../Components/Common/DefaultButton";
+import HabitsService from "../../Services/HabitsService";
 
 export default function HabitPage({ route }) {
 	const navigation = useNavigation();
@@ -19,6 +20,10 @@ export default function HabitPage({ route }) {
   const [timeNotification, setTimeNotification] = useState();
 
 	const { create, habit } = route.params;
+  const habitCreated = new Date();
+  const formatDate = `${habitCreated.getFullYear()}-${
+    habitCreated.getMonth() + 1
+  }-${habitCreated.getDate()}`;
 
   function handleCreateHabit() {
     if (
@@ -44,6 +49,24 @@ export default function HabitPage({ route }) {
         "Você precisa dizer a frequência e o horário da notificação!"
       );
     } else {
+      HabitsService.createHabit({
+        habitArea: habit?.habitArea,
+        habitName: habitInput,
+        habitFrequency: frequencyInput,
+        habitHasNotification: notificationToggle,
+        habitNotificationFrequency: dayNotification,
+        habitNotificationTime: timeNotification,
+        lastCheck: formatDate,
+        daysWithoutChecks: 0,
+        habitIsChecked: 0,
+        progressBar: 1,
+      }).then(() => {
+        Alert.alert("Sucesso na criação do hábito!");
+
+        navigation.navigate("Home", {
+          createdHabit: `Created in ${habit?.habitArea}`,
+        });
+      });
 	     navigation.navigate("Home", {
          createdHabit: `Created in ${habit?.habitArea}`,
        });
