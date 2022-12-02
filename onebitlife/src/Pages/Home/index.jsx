dcbe9d99cc4dd5265b0d0f466cb49d228a3981a1
Jefferson Pedro <jeffersonpedro05@gmail.com>
2022-12-02 00:39:26 -0300

    Renderização condicional de pagina e tempo de vida dinâmico

--- onebitlife/src/Pages/Home/index.jsx
+++ onebitlife/src/Pages/Home/index.jsx
@@ -6,9 +6,10 @@ import LifeStatus from "../../Components/Common/LifeStatus";
 import StatusBar from "../../Components/Home/StatusBar";
 import CreateHabit from "../../Components/Home/CreateHabit";
 import EditHabit from "../../Components/Home/EditHabit";
+import ChangeNavigationService from "../../Services/ChangeNavigationService";
 
 
-export default function Home(){
+export default function Home({route}){
 
     const navigation = useNavigation();
     const [mindHabit, setMindHabit] = useState();
@@ -16,16 +17,32 @@ export default function Home(){
     const [bodyHabit, setBodyHabit] = useState();
     const [funHabit, setFunHabit] = useState();
 
+    const [robotDaysLife, setRobotDaysLife] = useState();
+    const today = new Date();
+
     function handleNavExplanation(){
         navigation.navigate("AppExplanation")
     }
+    useEffect(() => {
+        ChangeNavigationService.checkShowHome(1)
+        .then((showHome) => {
+          const formDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
+          const checkDays =
+            new Date(formDate) - new Date(showHome.appStartData) + 1;
+    
+              setRobotDaysLife(checkDays.toString().padStart(2, "0"));
+        })
+          .catch((err) => console.log(err));
+      }, [route.params]);
 
     return(
         <View style = {styles.container}>
             <ScrollView>
                 <View style= {{ alignItems: "center" }}>
-                    <Text style={styles.dailyChecks}>❤️ 20 dias - 🚀 80 checks</Text>  
-
+                <Text style={styles.dailyChecks}>❤️ 
+                {robotDaysLife} {robotDaysLife === "01" ? "dia" : "dias"} - 🚀 80 Checks
+                </Text>
+                
                     <LifeStatus />
                     <StatusBar />
 
